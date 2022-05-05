e0f816f032c9093fee72c2f2e673740b3490f942
CT <tellezc007@gmail.com>
2022-05-05 15:09:54 -0700

    define columns within product

--- models/Product.js
+++ models/Product.js
@@ -7,17 +7,51 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
-  {
+Product.init({
     // define columns
-  },
-  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+
+    price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        validate: {
+            isDecimal: true
+        }
+    },
+
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10,
+        validate: {
+            isNumeric: true
+        }
+    },
+
+    category_id: {
+        type: DataTypes.INTEGER,
+        References: {
+            model: "catergory",
+            key: "id"
+        }
+    }
+
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
-);
+});
 
 module.exports = Product;
