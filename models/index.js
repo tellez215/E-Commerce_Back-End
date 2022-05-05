30c43f8df17f91018fb3f7517463f7dddbea6164
CT <tellezc007@gmail.com>
2022-05-05 16:13:01 -0700

    add associations to our files

--- models/index.js
+++ models/index.js
@@ -6,15 +6,33 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsto(Category, {
+    foreignKey: 'catergory_id'
+});
+
 // Categories have many Products
+
+Category.hasMany(product, {
+    foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+product.belongsToMany(tag, {
+    through: ProductTag,
+    foreignKey: 'product_id'
+});
+
 // Tags belongToMany Products (through ProductTag)
 
+tag.belongsToMany(product, {
+    through: ProductTag,
+    foreignKey: 'tag_id'
+});
+
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+    Product,
+    Category,
+    Tag,
+    ProductTag,
 };
