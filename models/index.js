0b9fd4323784d0cb4f8f301b5dbf28922abd00bf
CT <tellezc007@gmail.com>
2022-05-05 21:19:03 -0700

    add tag routes and add seeds

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 Product.belongsTo(Category, {
-    foreignKey: 'catergory_id'
+    foreignKey: 'category_id'
 });
 
 // Categories have many Products
